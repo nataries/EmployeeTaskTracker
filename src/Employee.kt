2c55d7e015827f8eb01525d2a3fbf8d227f5860f
nataries <natalizastavnaya@icloud.com>
2026-01-19 21:37:03 +0300

    Implement custom setters for salary and experience

--- src/Employee.kt
+++ src/Employee.kt
@@ -7,4 +7,38 @@ class Employee(fullName: String,
     private val _position = position
     private var _salary = salary
     private var _yearsOfExperience = yearsOfExperience
+
+    val fullName: String
+        get()=_fullName
+    val position: String
+        get() = _position
+    var salary: Int
+        get() = _salary
+        set(value) {
+            if(value < 0) {
+                println("Зарплата не может быть меньше 0!")
+            }
+            else {
+                _salary = value
+            }
+        }
+    var yearsOfExperience: Int
+        get() = _yearsOfExperience
+        set(value) {
+            _yearsOfExperience = value.coerceIn(0, 50)
+        }
+}
+
+fun main() {
+    val employee = Employee("Иванов Пётр Андреевич", "Менеджер", 50000, 5)
+    println(employee.fullName)
+    println(employee.position)
+    println(employee.salary)
+    println(employee.yearsOfExperience)
+
+    employee.salary = -5000
+    employee.yearsOfExperience = 120
+
+    println(employee.salary)
+    println(employee.yearsOfExperience)
 }
